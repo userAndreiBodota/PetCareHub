262b8e61cb2781f1a8dd1920c0ac74cc74c030a8
Andrei Bodota <bodotaandrei@gmail.com>
2024-10-05 17:32:14 +0800

    Initial commit for PetCareHub React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center bg-blue-500 p-6">
+      <h1 className="text-white text-3xl font-bold">Hello, Ma'am</h1>
     </div>
   );
 }
